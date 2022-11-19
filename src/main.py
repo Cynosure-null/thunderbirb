368b4921c67b4b77e3c4cb3a033a2c278eb6ee63
Cynosure-Null <cynosure@disroot.org>
2022-11-18 20:32:14 -0700

    less broke af

--- src/main.py
+++ src/main.py
@@ -5,37 +5,78 @@ from time import time
 import discord
 from dotenv import load_dotenv
 
+print("starting...")
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = 897945940273549343
+
+print("This security sucks ass")
+print(TOKEN)
 
 client = discord.Client()
 
 
-@client.event
+#@client.event
 async def on_ready():
-    for guild in client.guilds:
-        if guild.name == GUILD:
-            break
-
-    print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})\n'
-    )
+    print("Ready")
+    print("Logged in as a bot {0.user}".format(client))
 
 
 
 @client.event
 async def on_message(message):
-    decider = random.randint(0,5)
+    items = ["in the fab lab",
+             "in Wes\' car",
+             "in Erik\'s basement",
+             "in Kistler\'s meth lab",
+             "in the software dungeon",
+             "in the intestines of the goat",
+             "",
+             "on McMaster-Carr"
+             ]
+    decider = random.randint(0,40)
+    print(decider)
+    print("message recived")
 
-    if decider == 20:
-        if message.channel == 898004365716115467:
-            await message.channel.send("https://cdn.discordapp.com/attachments/898004365716115467/1035701847102591048/Screenshot_20221028-174833_Discord.jpg")
-            decider = random.randint(0,5)
+    username = str(message.author).split("#")[0]
+    channel = str(message.channel.name)
+    user_message = str(message.content)
+
+    print(f'Message {user_message} by {username} on {channel}')
+
+    if message.author == 272519236927225858 or message.author == 324286034382815254:
+        if decider == 2:
+            await message.channel.send("I use linux")
+
+
+    elif decider == 0 or decider == 1:
+        formatted_message = "Hey look what I found " + \
+            random.choice(items) + "\n" + \
+            "https://cdn.discordapp.com/attachments/898004365716115467/1035701847102591048/Screenshot_20221028-174833_Discord.jpg"
+
+        print(formatted_message)
+        await message.channel.send(formatted_message)
+        print("Operation complete. Target neutralized.")
+
+    elif "minors" in message.content.lower():
+        if decider == 3:
+            await message.channel.send("Ahem, " + random.choice(["Flip", "Croix"]))
+
+    elif "python" in message.content.lower():
+        if decider == 4:
+            await message.channel.send("await message.channel.send(\"Python sucks\") ")
+
+    elif "money" in message.content.lower():
+        await message.channel.send("Ben will pay for it")
+
+    elif "linux" in message.content.lower():
+        await message.channel.send("Windows is better")
+
+    elif "windows" in message.content.lower():
+        await message.channel.send("Linux is better")
+
     elif '&ping' in message.content.lower():
         await message.channel.send("pong")
 
 
-
+print("here")
 client.run(TOKEN)
